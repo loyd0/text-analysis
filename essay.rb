1886bb32036721e2e5775c5fd27c472401d602b1
Sam Loyd <Sam@192.168.1.106>
2017-03-16 17:57:51 +0000

    commented and new test file

--- essay.rb
+++ essay.rb
@@ -1,25 +1,28 @@
 
-essay = File.open("test.txt", "r")
+essay = File.open("accounting.txt", "r")
 essayContents = essay.read
 essayContents = essayContents.downcase
-# puts essayContents
-contents = essayContents.split(/\W+/)
-
-# print contents
 
 
+#Splits essayContents into words
+contents = essayContents.split(/[^[[:word:]]]+/)
+
+
+
+#Takes the array and sorts it into the most the frequent words.
 frequencies = Hash.new(0)
 contents.each { |word| frequencies[word] += 1 }
 frequencies = frequencies.sort_by {|a, b| b }
 frequencies.reverse!
 # frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+# ^ Outputs a total list
 
-# print frequencies
-
+#finds the words that have 4 or more characters
 keywords = frequencies.select {|a, b| a.length >= 4}
 
-puts "------- + " + keywords.length.to_s
 
+#selects first 5 keywords
 keywords = keywords.select.first(5)
 
 print keywords
+puts ""
