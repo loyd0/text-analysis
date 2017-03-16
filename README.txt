3e362748bccfd749e2399a38298592ab7d4ed179
Sam Loyd <Sam@192.168.1.106>
2017-03-16 16:07:35 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Text analysis tool - 
+
+Intended to produce top 5 frequently used words in a given set of text. 
+
+Ignores those connecting and short words (<=3) letters. 
+
---
+++ essay.rb
@@ -0,0 +1,17 @@
+
+essay = File.open("test.txt", "r")
+essayContents = essay.read
+essayContents = essayContents.downcase
+puts essayContents
+contents = essayContents.split(/\W+/)
+
+print contents
+
+
+frequencies = Hash.new(0)
+contents.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by {|a, b| b }
+frequencies.reverse!
+frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+
+print frequencies
