10f09b47173a6129fbc827136cf0a045a9703d19
Sam Loyd <Sam@192.168.1.106>
2017-03-16 16:39:36 +0000

    added selector to remove <4 char words

--- essay.rb
+++ essay.rb
@@ -2,16 +2,20 @@
 essay = File.open("test.txt", "r")
 essayContents = essay.read
 essayContents = essayContents.downcase
-puts essayContents
+# puts essayContents
 contents = essayContents.split(/\W+/)
 
-print contents
+# print contents
 
 
 frequencies = Hash.new(0)
 contents.each { |word| frequencies[word] += 1 }
 frequencies = frequencies.sort_by {|a, b| b }
 frequencies.reverse!
-frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+# frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
 
-print frequencies
+# print frequencies
+
+keywords = frequencies.select {|a, b| a.length >= 4}
+
+print keywords
